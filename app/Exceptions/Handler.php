3bfc4db2cb9790b7804e0bad237096282f12b930
Yosiak-alv <Josias_romero60@outlook.com>
2023-12-11 14:43:58 -0600

    Handler Erros added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Inertia\Inertia;
 
 class Handler extends ExceptionHandler
 {
@@ -27,4 +28,29 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    /**
+     * Prepare exception for rendering.
+     */
+    public function render($request, Throwable $e)
+    {
+        $response = parent::render($request, $e);
+
+        if (! app()->environment(['local', 'testing']) && in_array($response->status(), [500, 503, 404, 403])) {
+            return Inertia::render('Error', ['status' => $response->status()])
+                ->toResponse($request)
+                ->setStatusCode($response->status());
+        } elseif ($response->status() === 419) {
+            return back()->with([
+                'level' => 'warning',
+                'message' => 'La Pagina Expiro, Por Favor Intente De Nuevo.'
+            ]);
+        } elseif ($response->status() === 403) {
+            return back()->with([
+                'level' => 'error',
+                'message' => 'Esta Accion no esta Autorizada.'
+            ]);
+        }
+
+        return $response;
+    } 
 }
