2c332b309a665708955885e8b573bf1f015f0ef0
Yosiak-alv <Josias_romero60@outlook.com>
2023-12-13 21:34:59 -0600

    Fixing Seeders

--- database/seeders/WorkshopSeeder.php
+++ database/seeders/WorkshopSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\WorkShop;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\District;
 class WorkshopSeeder extends Seeder
 {
     /**
@@ -13,6 +13,31 @@ class WorkshopSeeder extends Seeder
      */
     public function run(): void
     {
-        WorkShop::factory(10)->create();
+        /* WorkShop::factory(10)->create(); */
+        $workShopNames = [
+            "Taller MotorPro",
+            "Garaje Rápido y Fiable",
+            "Mecánica Total",
+            "AutoMantenimiento",
+            "GaragePro",
+            "Mecánicos Expertos",
+            "Reparaciones de Confianza",
+            "Garaje Master",
+            "Taller de Autos Profesional",
+            "AutoCuidado",
+            "Mecánica en Acción",
+            "Taller de Emergencia",
+            "Garaje Veloz",
+            "Taller de Automóviles Premium",
+        ];
+        $district_ids = District::pluck('id')->toArray();
+        
+        WorkShop::factory(count($workShopNames ))->sequence(fn($sequence) => [
+            'name' => $workShopNames [$sequence->index],
+            'address' => fake()->address(),
+            'email' => fake()->unique()->email(),
+            'telephone' => fake()->numerify('########'),
+            'district_id' => fake()->randomElement($district_ids),
+        ])->create();
     }
 }
