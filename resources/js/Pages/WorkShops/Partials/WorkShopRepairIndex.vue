d8c30a7214615df01ca57d6b813abe67a84f3101
Yosiak-alv <Josias_romero60@outlook.com>
2024-01-16 20:48:36 -0600

    Fix Bug #WorkShop Repair if car is eliminated

--- resources/js/Pages/WorkShops/Partials/WorkShopRepairIndex.vue
+++ resources/js/Pages/WorkShops/Partials/WorkShopRepairIndex.vue
@@ -68,18 +68,18 @@ const hasPermission = (permissionName) => {
             <CardSection v-for="repair in props.repairs.data" class="text-base p-2">
                 <Link  v-if="hasPermission('ver reparacion')"
                     method="get" as="button" :href="route('repairs.show',repair.id)" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{{repair.car.model?.name}}, {{repair.car?.model.brand.name}},{{repair.work_shop_id}}</h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.car?.plates}}</p>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.car?.year}}</p>
+                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{{repair.car?.model?.name ?? 'Auto Eliminado'}}, {{repair.car?.model?.brand.name ?? ''}}</h5>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.car?.plates ?? ''}}</p>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.car?.year ?? ''}}</p>
                     <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.status?.name}}</p>
                     <span class="font-semibold">Total:</span> ${{ repair.total }}
                 </Link>
                 <div v-else
                     class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
                 >
-                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{{repair.car.model?.name}}, {{repair.car?.model.brand.name}}</h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.car?.plates}}</p>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.car?.year}}</p>
+                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{{repair.car?.model?.name ?? 'Auto Eliminado'}}, {{repair.car?.model?.brand.name ?? ''}}</h5>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.car?.plates ?? ''}}</p>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.car?.year ?? ''}}</p>
                     <p class="font-normal text-gray-700 dark:text-gray-400">{{repair.status?.name}}</p>
                     <span class="font-semibold">Total:</span> ${{ repair.total }}
                 </div>
