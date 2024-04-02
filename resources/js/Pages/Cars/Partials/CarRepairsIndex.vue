558855e4b1bfcf7150d2e2570e0312b527005fe1
Yosiak-alv <Josias_romero60@outlook.com>
2024-04-02 13:58:00 -0600

    Update CarRepairsIndex.vue

--- resources/js/Pages/Cars/Partials/CarRepairsIndex.vue
+++ resources/js/Pages/Cars/Partials/CarRepairsIndex.vue
@@ -4,7 +4,7 @@ import { Link, router, usePage } from '@inertiajs/vue3';
 import { ref, watch, computed, reactive } from 'vue';
 import { debounce } from 'lodash';
 import Paginator from '@/Components/Paginator.vue';
-
+import InputLabel from '@/Components/InputLabel.vue';
 const props = defineProps({
     carId:{
         type:Number,
@@ -49,34 +49,36 @@ const hasPermission = (permissionName) => {
         <h2 class="text-3xl font-semibold">Reparaciones</h2>
         <div class="flex flex-wrap justify-between items-center p-2 mt-4">
             <div class="mx-auto mt-2">
-                <div class="relative ">
+                <div class="relative">
                     <div class="absolute inset-y-0 left-29  flex items-center pl-3 pointer-events-none">
                         <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                     </div>
-                    <input type="text" id="search" v-model="form.search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Buscar">
+                    <input type="text" id="search" v-model="form.search" class="mt-4    bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Buscar">
                 </div>
             </div>
             <div class="mx-auto mt-2">
                 <div class="relative max-w-sm">
-                    <div class="absolute inset-y-0 end-0 flex items-center ps-3.5 mr-3 pointer-events-none">
+                    <div class="absolute inset-y-0 end-0 flex items-center ps-3.5 mr-3 mt-3.5 pointer-events-none">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                         </svg>
                     </div>
-                    <input type="date" v-model="form.start_date" class="bg-gray-50 border border-gray-300 text-gray-900 dark:text-white text-sm rounded-lg focus:border-red-500 dark:focus:border-red-600 focus:ring-red-500 dark:focus:ring-red-600 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" >
+                    <InputLabel for="start_date" value="Fecha Inicio: " />
+                    <input id="start_date" type="date" v-model="form.start_date" class="bg-gray-50 border border-gray-300 text-gray-900 dark:text-white text-sm rounded-lg focus:border-red-500 dark:focus:border-red-600 focus:ring-red-500 dark:focus:ring-red-600 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" >
                 </div>
             </div>
             <div class="mx-auto mt-2">
                 <div class="relative max-w-sm">
-                    <div class="absolute inset-y-0 end-0 flex items-center ps-3.5 mr-3 pointer-events-none">
+                    <div class="absolute inset-y-0 end-0 flex items-center ps-3.5 mr-3 mt-3.5 pointer-events-none">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                         </svg>
                     </div>
-                    <input type="date" v-model="form.end_date" class="bg-gray-50 border border-gray-300 text-gray-900 dark:text-white text-sm rounded-lg focus:border-red-500 dark:focus:border-red-600 focus:ring-red-500 dark:focus:ring-red-600 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" >
+                    <InputLabel for="end_date" value="Fecha Fin: " />
+                    <input id="end_date" type="date" v-model="form.end_date" class="bg-gray-50 border border-gray-300 text-gray-900 dark:text-white text-sm rounded-lg focus:border-red-500 dark:focus:border-red-600 focus:ring-red-500 dark:focus:ring-red-600 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" >
                 </div>
             </div>
-            <div class="space-x-2 space-y-2 mx-auto">
+            <div class="space-x-2 space-y-2 mx-auto mt-4">
                 <Link 
                     :href="route('cars.show',props.carId)"
                     method="get" as="button"
